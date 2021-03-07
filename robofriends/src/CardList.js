182c1f4ce8aa3ce52edbc1341ba4c96112bf2e9d
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-03-06 18:18:11 -1000

    changed variable to cardArr

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -2,13 +2,13 @@ import React from "react";
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardComp = robots.map((user, i) => {
+  const cardArr = robots.map((user, i) => {
     return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
 
   })
   return (
   <div>
-    {cardComp}
+    {cardArr}
   </div>
   );
 };
